bf7e3cd1b0236298b8933939bcdbc24202b7206d
Juan Basso <jrbasso@gmail.com>
2009-04-21 18:36:52 -0300

    Inclusao de metodo para busca de endereco atraves do CEP.

--- plugins/cake_br/tests/cases/models/behaviors/correios.test.php
+++ plugins/cake_br/tests/cases/models/behaviors/correios.test.php
@@ -19,6 +19,7 @@ class CakeBrCorreiosCase extends CakeTestCase {
 		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888-000', -12), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
 		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888-000', 10, true, -10), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
 		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888-000', 30.5), ERRO_CORREIOS_EXCESSO_PESO);
+		// Dados obtidos dia 21/04/2009
 		$correios = $this->Correios->valorFrete(CORREIOS_SEDEX, '88037-100', '88037-100', 10, true, 30, false);
 		$correto = array(
 			'ufOrigem' => 'SC',
@@ -29,6 +30,17 @@ class CakeBrCorreiosCase extends CakeTestCase {
 			'valorTarifaValorDeclarado' => 0,
 			'valorFrete' => 22.5,
 			'valorTotal' => 25.8
+		);
+		$this->assertEqual($correios, $correto);
+	}
+
+	function testEndereco() {
+		$correios = $this->Correios->endereco('88037-100');
+		$correto = array(
+			'logradouro' => 'Rua Acadêmico Reinaldo Consoni',
+			'bairro' => 'Santa Mônica',
+			'cidade' => 'Florianópolis',
+			'uf' => 'SC'
 		);
 		$this->assertEqual($correios, $correto);
 	}
