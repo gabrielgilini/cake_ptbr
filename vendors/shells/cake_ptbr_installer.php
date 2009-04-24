4ca9054754c371c136e175aebedf5b5e708dcc5c
Juan Basso <jrbasso@gmail.com>
2009-04-23 21:37:55 -0300

    Ajuste do instalador do PluginManager para informar caso o usuário acesse via shell.

--- vendors/shells/cake_ptbr_installer.php
+++ vendors/shells/cake_ptbr_installer.php
@@ -1,22 +1,29 @@
 <?php
 
-App::import('Vendor', 'PluginManager.Installer');
-
-class CakePtbrInstaller extends InstallerPM {
-
-	function install() {
-		App::import('Core', 'File');
-		$bootstrap = new File(CONFIGS . 'bootstrap.php');
-		$conteudo = $bootstrap->read();
-		// Caso o plugin já esteja instalado, ignora
-		if (strpos('cake_ptbr', $conteudo) !== false) {
-			return;
+if (App::import('Vendor', 'PluginManager.Installer')) {
+	class CakePtbrInstaller extends InstallerPM {
+		function install() {
+			App::import('Core', 'File');
+			$bootstrap = new File(CONFIGS . 'bootstrap.php');
+			$conteudo = $bootstrap->read();
+			// Caso o plugin já esteja instalado, ignora
+			if (strpos('cake_ptbr', $conteudo) !== false) {
+				return;
+			}
+			$conteudo = str_replace('?>', "require APP . 'plugins' . DS . 'cake_ptbr' . DS . 'config' . DS . 'bootstrap.php';\n?>", $conteudo);
+			$bootstrap->write($conteudo);
+			$bootstrap->close();
 		}
-		$conteudo = str_replace('?>', "require APP . 'plugins' . DS . 'cake_ptbr' . DS . 'config' . DS . 'bootstrap.php';\n?>", $conteudo);
-		$bootstrap->write($conteudo);
-		$bootstrap->close();
 	}
+}
 
+class CakePtbrInstallerShell extends Shell {
+	function main() {
+		$this->out('Este arquivo eh reservado para instalacao atraves do PluginManager.');
+		$this->out('Mais informacoes sobre o PluginManager: http://kiss.souagil.com.br/2009/04/plugin-manager/');
+		$this->out('');
+		$this->out('Desculpe o transtorno.');
+	}
 }
 
 ?>
