17772260cc240dda3e50e1b709d9a9da10c326be
Juan Basso <jrbasso@gmail.com>
2010-03-12 22:37:07 -0300

    Incluindo suporte ao LC_TIME. Testes foram incluidos tambem.

--- tests/cases/config/i18n.test.php
+++ tests/cases/config/i18n.test.php
@@ -16,6 +16,32 @@ class CakePtbrI18nCase extends CakeTestCase {
 		$this->assertEqual(__('Missing Component File', true), 'Arquivo de Component não encontrado');
 		$this->assertEqual(__d('default', 'Missing Database', true), 'Database não encontrado');
 	}
+
+	function testTimeDefinition() {
+		$result = __c('abday', 5, true);
+		$expected = array('Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab');
+		$this->assertEqual($result, $expected);
+
+		$result = __c('day', 5, true);
+		$expected = array('Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado');
+		$this->assertEqual($result, $expected);
+
+		$result = __c('abmon', 5, true);
+		$expected = array('Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez');
+		$this->assertEqual($result, $expected);
+
+		$result = __c('mon', 5, true);
+		$expected = array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro');
+		$this->assertEqual($result, $expected);
+
+		$result = __c('d_fmt', 5, true);
+		$expected = '%d/%m/%Y';
+		$this->assertEqual($result, $expected);
+
+		$result = __c('am_pm', 5, true);
+		$expected = array('AM', 'PM');
+		$this->assertEqual($result, $expected);
+	}
 }
 
 ?>
