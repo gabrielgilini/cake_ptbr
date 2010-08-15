5aecfaffdf237ee7c08d0c60003a9a94a44d89f5
Juan Basso <jrbasso@gmail.com>
2010-08-15 18:25:17 -0300

    Documentacao do helper Formatacao.

--- views/helpers/formatacao.php
+++ views/helpers/formatacao.php
@@ -12,10 +12,22 @@
 
 class FormatacaoHelper extends AppHelper {
 
+/**
+ * Helpers auxiliares
+ *
+ * @var array
+ * @access public
+ */
 	var $helpers = array('Time', 'Number');
 
-	/* Datas */
-
+/**
+ * Formata a data
+ *
+ * @param integer $data Data em timestamp ou null para atual
+ * @param array $opcoes É possível definir o valor de 'invalid' e 'userOffset' que serão usados pelo helper Time
+ * @return string Data no formato dd/mm/aaaa
+ * @access public
+ */
 	function data($data = null, $opcoes = array()) {
 		$padrao = array(
 			'invalid' => '31/12/1969',
@@ -27,6 +39,14 @@ class FormatacaoHelper extends AppHelper {
 		return $this->Time->format('d/m/Y', $data, $config['invalid'], $config['userOffset']);
 	}
 
+/**
+ * Formata a data e hora
+ *
+ * @param integer $dataHora Data e hora em timestamp ou null para atual
+ * @param boolean $segundos Mostrar os segundos
+ * @return string Data no formato dd/mm/aaaa hh:mm:ss
+ * @access public
+ */
 	function dataHora($dataHora = null, $segundos = true) {
 		$dataHora = $this->_ajustaDataHora($dataHora);
 		if ($segundos) {
@@ -35,6 +55,13 @@ class FormatacaoHelper extends AppHelper {
 		return $this->Time->format('d/m/Y H:i', $dataHora);
 	}
 
+/**
+ * Mostrar a data completa
+ *
+ * @param integer $dataHora Data e hora em timestamp ou null para atual
+ * @return string Descrição da data no estilo "Sexta-feira", 01 de Janeiro de 2010, 00:00:00"
+ * @access public
+ */
 	function dataCompleta($dataHora = null) {
 		$_diasDaSemana = array('Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado');
 		$_meses = array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro');
@@ -46,6 +73,13 @@ class FormatacaoHelper extends AppHelper {
 		return sprintf('%s, %02d de %s de %04d, %s', $_diasDaSemana[$w], date('d', $dataHora), $_meses[$n], date('Y', $dataHora), date('H:i:s', $dataHora));
 	}
 
+/**
+ * Se a data for nula, usa data atual
+ *
+ * @param mixed $data
+ * @return integer Se null, retorna a data/hora atual
+ * @access protected
+ */
 	function _ajustaDataHora($data) {
 		if (is_null($data)) {
 			return time();
@@ -53,16 +87,38 @@ class FormatacaoHelper extends AppHelper {
 		return $data;
 	}
 
-	/* Números */
-
+/**
+ * Número float com ponto ao invés de vírgula
+ *
+ * @param float $numero Número
+ * @param integer $casasDecimais Número de casas decimais
+ * @return string Número formatado
+ * @access public
+ */
 	function precisao($numero, $casasDecimais = 3) {
 		return sprintf("%01.{$casasDecimais}f", $numero);
 	}
 
+/**
+ * Valor formatado com símbolo de %
+ *
+ * @param float $numero Número
+ * @param integer $casasDecimais Número de casas decimais
+ * @return string Número formatado com %
+ * @access public
+ */
 	function porcentagem($numero, $casasDecimais = 2) {
 		return $this->precisao($numero, $casasDecimais) . '%';
 	}
 
+/**
+ * Formata um valor para reais
+ *
+ * @param float $valor Valor
+ * @param array $opcoes Mesmas opções de Number::currency()
+ * @return string Valor formatado em reais
+ * @access public
+ */
 	function moeda($valor, $opcoes = array()) {
 		$padrao = array(
 			'before'=> 'R$ ',
@@ -89,8 +145,15 @@ class FormatacaoHelper extends AppHelper {
 		return $this->Number->currency($valor, null, $config);
 	}
 
+/**
+ * Valor por extenso em reais
+ *
+ * @param float $numero
+ * @return string Valor em reais por extenso
+ * @access public
+ * @link http://forum.imasters.uol.com.br/index.php?showtopic=125375
+ */
 	function moedaPorExtenso($numero) {
-		// Adaptado de http://forum.imasters.uol.com.br/index.php?showtopic=125375
 		$singular = array('centavo', 'real', 'mil', 'milhão', 'bilhão', 'trilhão', 'quatrilhão');
 		$plural = array('centavos', 'reais', 'mil', 'milhões', 'bilhões', 'trilhões', 'quatrilhões');
 
