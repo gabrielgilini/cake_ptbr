7f3afeb9c9957d7dba053b6222533066e8c725a8
Juan Basso <jrbasso@gmail.com>
2010-08-15 21:50:20 -0300

    Removido 2 testes que sao invalidos. Os Correios aceitam CEPs sem o traco, ex.: 88000000.

--- tests/cases/models/behaviors/correios.test.php
+++ tests/cases/models/behaviors/correios.test.php
@@ -153,8 +153,6 @@ class CakePtbrCorreiosCase extends CakeTestCase {
 	function testValorFrete() {
 		$this->assertNotEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888-000', 10), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
 		$this->assertEqual($this->Correios->valorFrete(40000, '88000-000', '88888-000', 10), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
-		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000000', '88888-000', 10), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
-		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888000', 10), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
 		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888-000', 'peso'), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
 		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888-000', -12), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
 		$this->assertEqual($this->Correios->valorFrete(CORREIOS_SEDEX, '88000-000', '88888-000', 10, true, -10), ERRO_CORREIOS_PARAMETROS_INVALIDOS);
